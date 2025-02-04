ca10bf5977a6aa10379b18ba0f5ad25464b16d09
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-02-04 18:17:00 +0100

    Update config

--- editor.js
+++ editor.js
@@ -1,4 +1,4 @@
-require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs' }});
+require.config({ paths: { 'vs': '/vs' }});
 require(['vs/editor/editor.main'], function() {
     window.htmlEditor = monaco.editor.create(document.getElementById('html'), {
         language: 'html',
